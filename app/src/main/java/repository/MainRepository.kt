d91fd7b98c6565b21667fe274851cfdf664f025b
Dimitris Ligkitsakos <dimitrisligi@gmail.com>
2021-08-06 11:54:49 +0300

    Adding Repository

--- app/src/main/java/repository/MainRepository.kt
+++ app/src/main/java/repository/MainRepository.kt
@@ -1,4 +1,36 @@
 package repository
 
+import androidx.lifecycle.LiveData
+import api.ApiBuilder
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import models.UsersDomainModel
+
 object MainRepository {
+
+    private var currentJob: CompletableJob? = null
+
+    fun getUsers():LiveData<UsersDomainModel>{
+        currentJob = Job()
+        return object : LiveData<UsersDomainModel>(){
+            override fun onActive() {
+                super.onActive()
+                currentJob?.let{
+                    CoroutineScope(IO+it).launch {
+                        val users = ApiBuilder.apiService.getUsers()
+                        withContext(Main){
+                            value = users
+                            it.complete()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    fun cancelJobs(){
+        currentJob?.cancel()
+    }
 }
